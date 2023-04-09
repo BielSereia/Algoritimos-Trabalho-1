b54e85cd13690e880bc4d9d004173fa5064c8538
Gabriel Antonio Sereia <bielsereia2@hotmail.com>
2023-04-09 15:08:18 +0000

    Marcilio Dias Completo

--- main.cpp
+++ main.cpp
@@ -38,6 +38,75 @@ int main() {
       cout << "Parabéns " << nome << "!\n";
       cout << "Seu contrato foi assinado! O técnico deseja boa sorte na "
               "caminhada e espera contar com você na temporada. \n";
+
+      cout << "Após o primeiro mês de treino, suas chuteiras estão desgatadas. "
+              "Você deseja comprar outra?\n";
+      cout << "1 - Sim\n";
+      cout << "2 - Não\n";
+
+      cin >> escolha;
+
+      if (escolha == 1) {
+        cout << "Após se destacar bastante nos treinos e nos jogos durante a "
+                "temporada, o clube decide renovar seu contrato e aumentando "
+                "seu salário para três mil reais.\n";
+        cout << "Cada vez mais, você vem se destacando, sendo mais notado por "
+                "outros clubes.\n";
+        cout << "Chegando até você uma proposta do Vasco da Gama.\n";
+        cout << "1 - Aceitar entrar para o Vasco.\n";
+        cout << "2 - Recusar entrar para o Vasco.\n";
+
+        cin >> escolha;
+
+        if (escolha == 1) {
+          cout << "Após o seu primeiro jogo pelo Vasco, você dá uma entrevista "
+                  "pós jogo.\n";
+          cout << "1 - Detonar o time adversário, expondo briga com técnico.\n";
+          cout << "2 - Exaltar o time adversário, e elogia o seu time.\n";
+
+          cin >> escolha;
+
+          if (escolha == 1) {
+            cout << "A diretoria do clube não gostou nem um pouco do seu comportamento.\n";
+            cout << "E com a falta de comportamento e péssimo equilibrio psicológico, o clube decide encerrar o contrato.\n";
+            cout << "Chegando assim, ao fim da sua carreira.\n";
+            
+            return 0;
+          } else if (escolha == 2) {
+            cout << "Mostrando ser uma pessoa de bom carater, os holofotes começam a virar para você, alavancando sua carreira.\n";
+            cout << "Você foca bastante nos treinos e em ser uma pessoa melhorar, chegando ao ponto mais alto da sua carreira.\n";
+            cout << "Completando com sucesso, o seu sonho!\n";
+
+            return 0;
+          } else {
+            cout << "Escolha Invalida!\n";
+            return 0;
+          }
+        } else if (escolha == 2) {
+          cout << "Recusando o convite para o Vasco, a sua imagem fica manchada, você e seu agente até tenta resolver isso, mas não conseguem.\n";
+          cout << "Deixando você apenas com a opção de continuar no Marcilio.\n";
+          cout << "Como seu objetivo era evoluir, você acaba desistindo de seu sonho.\n";
+
+          return 0;
+        } else {
+          cout << "Escolha Invalida!\n";
+          return 0;
+        }
+      } else if (escolha == 2) {
+        cout << "No treino, você não consegue desempenhar tão bem, e em campo, muito menos.\n";
+        cout << "Então, o técnico decide te deixar no banco.\n";
+        cout << "Chateado com a decisão do técnico, você decide ir em uma festa com seus colegas de equipe, para se divertir um pouco e esquecer do ocorrido.\n";
+        cout << "Nessa noitada, você perde o controle, e bebe exageradamente.\n";
+        cout << "No outro dia, você se atrasa para o treino, e chega bebado.\n";
+        cout << "O técnico e o clube não gostam desse comportamento, e após uma descussão entre você a comissão tecnica, decidem te desligar do clube.\n";
+        cout << "Chegando assim, ao fim da sua carreira.\n";
+
+        return 0;
+      } else {
+        cout << "Escolha Invalida!\n";
+        return 0;
+      }
+
     } else if (escolha == 2) {
       cout << "Parabéns " << nome << "!\n";
       cout << "Seu contrato foi assinado! O técnico deseja boa sorte na "
@@ -60,7 +129,28 @@ int main() {
       cin >> escolha;
 
       if (escolha == 1) {
+            cout << "Para melhorar, você tem que escolher entre:\n";
+            cout << "1 - Treinar no seus dias de folga.\n";
+            cout << "2 - Aproveitar seus dias de folga com a familia\n";
 
+            cin >> escolha;
+
+            if (escolha == 1) {
+              cout << "Na tentativa de melhorar, você acaba forcando muito seu "
+                      "corpo, e se lesiona, precisando ficar 4 meses parado.\n";
+              cout << "Com isso, o clube decide cancelar seu contrato, visto "
+                      "que estava dando só prejuizo.\n";
+              return 0;
+            } else if (escolha == 2) {
+              cout << "Aproveitando os dias de folga, você não consegue "
+                      "melhorar o tanto que gostaria, e chegando ao fim da "
+                      "temporada, o clube vê que seu desempenho não é um dos "
+                      "melhores, e acaba encerrando o contrato.\n";
+              return 0;
+            } else {
+              cout << "Escolha Invalida!\n";
+              return 0;
+            }
       } else if (escolha == 2) {
         cout << "Nessa noite, os seus companheiros de time irão a uma festa "
                 "essa noite e te convidaram para ir, disseram que a noitada "
@@ -107,7 +197,8 @@ int main() {
           cin >> escolha;
 
           if (escolha == 1) {
-            cout << "Um tempo se passa, e você melhora, porém sente que precisa melhorar mais, consequentimente treinar mais.\n";
+            cout << "Um tempo se passa, e você melhora, porém sente que "
+                    "precisa melhorar mais, consequentimente treinar mais.\n";
             cout << "Focado em melhorar, você fica em duvida entre:\n";
             cout << "1 - Treinar no seus dias de folga.\n";
             cout << "2 - Aproveitar seus dias de folga com a familia\n";
@@ -115,11 +206,21 @@ int main() {
             cin >> escolha;
 
             if (escolha == 1) {
-              cout << "Na tentativa de melhorar, você acaba forcando muito seu corpo, e se lesiona, precisando ficar 4 meses parado.\n";
-              cout << "Com isso, o clube decide cancelar seu contrato, visto que estava dando só prejuizo.\n";
+              cout << "Na tentativa de melhorar, você acaba forcando muito seu "
+                      "corpo, e se lesiona, precisando ficar 4 meses parado.\n";
+              cout << "Com isso, o clube decide cancelar seu contrato, visto "
+                      "que estava dando só prejuizo.\n";
+              return 0;
+            } else if (escolha == 2) {
+              cout << "Aproveitando os dias de folga, você não consegue "
+                      "melhorar o tanto que gostaria, e chegando ao fim da "
+                      "temporada, o clube vê que seu desempenho não é um dos "
+                      "melhores, e acaba encerrando o contrato.\n";
+              return 0;
+            } else {
+              cout << "Escolha Invalida!\n";
               return 0;
             }
-            
           } else if (escolha == 2) {
             cout << "Conformado com a reserva e com as poucas entradas em "
                     "campo, assim fica até o final da temporada, e com o final "
@@ -132,7 +233,6 @@ int main() {
             cout << "Escolha Invalida!\n";
             return 0;
           }
-
         } else {
           cout << "Escolha Invalida!\n";
           return 0;
